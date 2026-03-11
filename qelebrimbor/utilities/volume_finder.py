334e91db6112ebacd8249a195bf8af2e21150042
Seweryn Dynerowicz <seweryn.dynerowicz@gmail.com>
2026-03-11 18:15:21 +0100

    Added volume_finder package for exhaustive search.

---
+++ qelebrimbor/utilities/volume_finder.py
@@ -0,0 +1,41 @@
+from logging import getLogger
+console = getLogger(__name__)
+
+from qelebrimbor.helpers.spacetime import Spacetime, Coordinates
+from qelebrimbor.common.components_bg import CubeKind
+
+from qelebrimbor.pathfinders.pathfinder_dfs import find_paths_dfs
+
+class VolumeFinder:
+    source = (CubeKind.XZZ, Spacetime.ORIGIN)
+
+    @staticmethod
+    def find_path_overhead(target: tuple[CubeKind, Coordinates]):
+        discovered_paths = find_paths_dfs(target, VolumeFinder.source, extra_volume = 12)
+        minimal_volume = min(discovered_paths.keys())
+        _, target_position = target
+        manhattan_distance = Spacetime.ORIGIN.get_manhattan_distance(target_position)
+        differential = minimal_volume - manhattan_distance
+
+        return differential
+
+        # Target : [ XZZ ]
+        # INFO:__main__:Statistics for MD=2 / cases=6 : defaultdict(<class 'int'>, {2: 3, 0: 3})
+        # INFO:__main__:Statistics for MD=3 / cases=10 : defaultdict(<class 'int'>, {0: 6, 2: 4})
+        # INFO:__main__:Statistics for MD=4 / cases=15 : defaultdict(<class 'int'>, {0: 11, 2: 4})
+        # INFO:__main__:Statistics for MD=5 / cases=21 : defaultdict(<class 'int'>, {2: 4, 0: 17})
+        # INFO:__main__:Statistics for MD=6 / cases=28 : defaultdict(<class 'int'>, {2: 4, 0: 24})
+        # INFO:__main__:Statistics for MD=7 / cases=36 : defaultdict(<class 'int'>, {0: 32, 2: 4})
+        # INFO:__main__:Statistics for MD=8 / cases=45 : defaultdict(<class 'int'>, {0: 41, 2: 4})
+        # INFO:__main__:Statistics for MD=9 / cases=55 : defaultdict(<class 'int'>, {0: 51, 2: 4})
+        # INFO:__main__:Statistics for MD=10 / cases=66 : defaultdict(<class 'int'>, {0: 62, 2: 4})
+
+        # Target : all
+        # INFO:__main__:Statistics for MD<=1 / cases=18  : defaultdict(<class 'int'>, {0:  4, 2:  5, 4:5, 6:4})   ~22%
+        # INFO:__main__:Statistics for MD<=2 / cases=36  : defaultdict(<class 'int'>, {0: 11, 2: 19, 4:6})        ~30%
+        # INFO:__main__:Statistics for MD<=3 / cases=78  : defaultdict(<class 'int'>, {0: 29, 2: 35, 4:10, 6:4})  ~37%
+        # INFO:__main__:Statistics for MD<=4 / cases=126 : defaultdict(<class 'int'>, {0: 57, 2: 58, 4:11})       ~45%
+        # INFO:__main__:Statistics for MD<=5 / cases=204 : defaultdict(<class 'int'>, {0:102, 2: 83, 4:15, 6:4})  ~50%
+        # INFO:__main__:Statistics for MD<=6 / cases=294 : defaultdict(<class 'int'>, {0:163, 2:115, 4:16})       ~55%
+        # INFO:__main__:Statistics for MD<=7 / cases=420 : defaultdict(<class 'int'>, {0:247, 2:149, 4:20, 6:4})  ~59%
+        # INFO:__main__:Statistics for MD<=8 / cases=564 : defaultdict(<class 'int'>, {2:190, 0:353, 4:21})       ~63%
--- void-explorer.py
+++ void-explorer.py
@@ -1,82 +1,46 @@
 import logging
-
-from qelebrimbor.pathfinders.pathfinder_bfs import find_paths_bfs
-from qelebrimbor.pathfinders.pathfinder_dfs import find_paths_dfs
-
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 console = logging.getLogger(__name__)
-console.setLevel(logging.INFO)
 
 from collections import defaultdict
 
 from qelebrimbor.common.components_bg import CubeKind
 from qelebrimbor.helpers.spacetime import Spacetime
+from qelebrimbor.utilities.volume_finder import VolumeFinder
 
 logging.getLogger('qelebrimbor.helpers').setLevel(logging.CRITICAL)
 logging.getLogger('qelebrimbor.pathfinders').setLevel(logging.CRITICAL)
+logging.getLogger('qelebrimbor.utilities').setLevel(logging.INFO)
+
+MOVE_ABOVE = Spacetime.XM + Spacetime.ZP
+MOVE_RIGHT = Spacetime.XM + Spacetime.YP
 
 if __name__ == "__main__":
-
-    MD = 12
-    START = MD * Spacetime.XP
-    MOVE_ABOVE = Spacetime.XM + Spacetime.ZP
-    MOVE_RIGHT = Spacetime.XM + Spacetime.YP
-
+    manhattan_distance = 6
+    start_position = manhattan_distance * Spacetime.XP
     # The following seem to follow from symmetry relative to the source Cube
     # > Conjecture 1 : CubeKind.ZZX yields the same outcomes as CubeKind.ZXZ
     # > Conjecture 2 : CubeKind.XXZ yields the same outcomes as CubeKind.XZX
-    kinds = [ CubeKind.XZZ, CubeKind.ZXZ, CubeKind.ZZX, CubeKind.ZXX, CubeKind.XZX, CubeKind.XXZ ]
-    positions = [ (xy, z) for z in range(MD+1) for xy in range(MD - z + 1) ]
+    kinds = [CubeKind.XZZ, CubeKind.ZXZ, CubeKind.ZZX, CubeKind.ZXX, CubeKind.XZX, CubeKind.XXZ]
+    positions = [(xy, z) for z in range(manhattan_distance + 1) for xy in range(manhattan_distance - z + 1)]
 
-    total_kinds = len(kinds)
-    total_positions = len(positions)
-    console.info(f"Kinds [{total_kinds}]")
-    console.info(f"Positions [{total_positions}]")
-
-    console.info(f"Manhattan Distance : {MD}")
     for target_kind in kinds:
         count = 1
         statistics = defaultdict(int)
         console.info(f"Target kind : {target_kind}")
         overheads = defaultdict(int)
         for xy, z in positions:
-            target_position = START + z * MOVE_ABOVE + xy * MOVE_RIGHT
-            target = (target_kind, target_position)
-            discovered_paths = find_paths_dfs(target, extra_volume = 12)
-            minimal_volume = min(discovered_paths.keys())
-            manhattan_distance = Spacetime.ORIGIN.get_manhattan_distance(target_position)
-            differential = minimal_volume - manhattan_distance
-            console.debug(f"{count}/{total_positions}> @{target_position} [min-vol={minimal_volume}/man-dis={manhattan_distance}] : volume +{differential}")
-            # for mv, path in discovered_paths.items(): console.info(f"> {path}")
-            statistics[differential] += 1
-            overheads[xy, z] = differential
+            target_position = start_position + z * MOVE_ABOVE + xy * MOVE_RIGHT
+            overhead = VolumeFinder.find_path_overhead( (target_kind, target_position) )
+
+            statistics[overhead] += 1
+            overheads[xy, z] = overhead
+
             count += 1
-        percentage = 100.0 * statistics[0] / total_positions
+        percentage = 100.0 * statistics[0] / len(positions)
         console.info(f"> Zero-overhead percentage : {percentage:.2f}% [{statistics}]")
-        for current_z in reversed(range(MD+1)):
+        for current_z in reversed(range(manhattan_distance + 1)):
             report = current_z * ' '
-            for current_xy in range(MD - current_z + 1):
+            for current_xy in range(manhattan_distance - current_z + 1):
                 report += f" {overheads[current_xy, current_z]}"
             console.info(f">> Overhead :{report}")
-        # console.info(f"> Statistics for MD={MD} / cases={total_positions} : {statistics}")
-
-    # Target : [ XZZ ]
-    # INFO:__main__:Statistics for MD=2 / cases=6 : defaultdict(<class 'int'>, {2: 3, 0: 3})
-    # INFO:__main__:Statistics for MD=3 / cases=10 : defaultdict(<class 'int'>, {0: 6, 2: 4})
-    # INFO:__main__:Statistics for MD=4 / cases=15 : defaultdict(<class 'int'>, {0: 11, 2: 4})
-    # INFO:__main__:Statistics for MD=5 / cases=21 : defaultdict(<class 'int'>, {2: 4, 0: 17})
-    # INFO:__main__:Statistics for MD=6 / cases=28 : defaultdict(<class 'int'>, {2: 4, 0: 24})
-    # INFO:__main__:Statistics for MD=7 / cases=36 : defaultdict(<class 'int'>, {0: 32, 2: 4})
-    # INFO:__main__:Statistics for MD=8 / cases=45 : defaultdict(<class 'int'>, {0: 41, 2: 4})
-    # INFO:__main__:Statistics for MD=9 / cases=55 : defaultdict(<class 'int'>, {0: 51, 2: 4})
-    # INFO:__main__:Statistics for MD=10 / cases=66 : defaultdict(<class 'int'>, {0: 62, 2: 4})
-
-    # Target : all
-    # INFO:__main__:Statistics for MD<=1 / cases=18  : defaultdict(<class 'int'>, {0:  4, 2:  5, 4:5, 6:4})   ~22%
-    # INFO:__main__:Statistics for MD<=2 / cases=36  : defaultdict(<class 'int'>, {0: 11, 2: 19, 4:6})        ~30%
-    # INFO:__main__:Statistics for MD<=3 / cases=78  : defaultdict(<class 'int'>, {0: 29, 2: 35, 4:10, 6:4})  ~37%
-    # INFO:__main__:Statistics for MD<=4 / cases=126 : defaultdict(<class 'int'>, {0: 57, 2: 58, 4:11})       ~45%
-    # INFO:__main__:Statistics for MD<=5 / cases=204 : defaultdict(<class 'int'>, {0:102, 2: 83, 4:15, 6:4})  ~50%
-    # INFO:__main__:Statistics for MD<=6 / cases=294 : defaultdict(<class 'int'>, {0:163, 2:115, 4:16})       ~55%
-    # INFO:__main__:Statistics for MD<=7 / cases=420 : defaultdict(<class 'int'>, {0:247, 2:149, 4:20, 6:4})  ~59%
-    # INFO:__main__:Statistics for MD<=8 / cases=564 : defaultdict(<class 'int'>, {2:190, 0:353, 4:21})       ~63%
