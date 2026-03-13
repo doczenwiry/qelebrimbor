fa3aa0342cbfb55512eac534f5ffcaf552858881
Seweryn Dynerowicz <seweryn.dynerowicz@gmail.com>
2026-03-13 13:50:56 +0100

    Altered octahedron exploration to print all sides' overheads side-by-side.

--- exploration/octahedron-sides-explorer.py
+++ exploration/octahedron-sides-explorer.py
@@ -29,31 +29,39 @@ def __convert_direction(axis: str, direction: int):
     else:
         raise ValueError("Direction must be +1/-1.")
 
-def __make_overhead_header(face: Octant, z: int):
+def __make_overhead_delimiter(face: Octant, z: int, header: bool = True):
     if z == 0:
         xd, yd, zd = face.value
-        point_l = __convert_direction("X", xd) if xd == yd else __convert_direction("Y", yd)
-        point_r = __convert_direction("Y", yd) if xd == yd else __convert_direction("X", xd)
-        hdr = f"{point_l}--{point_r}"
+        # if header:
+        hdr = f" {__convert_direction("X", xd) if xd == yd else __convert_direction("Y", yd)} "
+        # else:
+        #     hdr = f"{__convert_direction("Y", yd) if xd == yd else __convert_direction("X", xd)}"
     elif z == manhattan_distance:
-        hdr = "  ZP  "
+        hdr = " ZP "
     elif z == -manhattan_distance:
-        hdr = "  ZM  "
+        hdr = " ZM "
     else:
-        hdr = "      "
+        hdr = "    "
 
     return hdr
 
-def __layout_overheads(faces: tuple[Octant, Octant], manhattan_distance: int, overheads: defaultdict[Coordinates, int]):
+def __make_overheads_layout(
+        faces: tuple[Octant, Octant],
+        manhattan_distance: int,
+        overheads: defaultdict[Coordinates, int]
+):
     target_face = faces[0]
+    layout = {}
     for z in range(manhattan_distance, -manhattan_distance-1, -1):
-        oh_row = abs(z) * ' '
+        oh_row  = abs(z) * ' '
         current_row = sorted(filter(lambda p : p.z == z, overheads.keys()), key = SORTING_FUNCTIONS[target_face])
         for pos in current_row:
             oh = overheads[pos]
             oh_row += f" {oh}"
-        hdr = __make_overhead_header(faces[0], z)
-        console.info(f"{hdr}>{oh_row}")
+        oh_row += abs(z) * ' '
+        hdr = __make_overhead_delimiter(faces[0], z, header = True)
+        layout[z] = f"{hdr}>   {oh_row}   <"
+    return layout
 
 def __cmp_xp_yp(position1: Coordinates, position2: Coordinates):
     return -1 if position1.x > position2.x and position1.y < position2.y else +1
@@ -80,8 +88,8 @@ SORTING_FUNCTIONS = {
 
 SIDE = {
     "PP" : ( Octant.PPP, Octant.PPM ),
-    "MP" : ( Octant.MPP, Octant.MPM ),
     "PM" : ( Octant.PMP, Octant.PMM ),
+    "MP" : ( Octant.MPP, Octant.MPM ),
     "MM" : ( Octant.MMP, Octant.MMM )
 }
 
@@ -90,11 +98,16 @@ if __name__ == "__main__":
     # The following seem to follow from symmetry relative to the source Cube
     # > Conjecture 1 : CubeKind.ZZX yields the same outcomes as CubeKind.ZXZ
     # > Conjecture 2 : CubeKind.XXZ yields the same outcomes as CubeKind.XZX
-    kinds = [ CubeKind.XZZ , CubeKind.ZXZ, CubeKind.ZZX, CubeKind.ZXX, CubeKind.XZX, CubeKind.XXZ ]
+    kinds = [ CubeKind.XZZ, CubeKind.ZXZ, CubeKind.ZZX, CubeKind.ZXX, CubeKind.XZX, CubeKind.XXZ ]
     sides = [ "PP", "MP", "MM", "PM" ]
+
+    source_kind, source_position = VolumeFinder.SOURCE
+    console.info(f"Source kind : {source_kind}@{source_position}")
+    console.info(f"> Manhattan Distance : {manhattan_distance}")
 
     for target_kind in kinds:
         console.info(f"Target kind : {target_kind}")
+        target_side_layouts = {}
         for target_side in sides:
             count = 1
             statistics: defaultdict[int, int] = defaultdict(int)
@@ -111,5 +124,12 @@ if __name__ == "__main__":
 
                 count += 1
             percentage = 100.0 * statistics[0] / len(positions)
-            console.info(f"{target_side}> Zero-overhead percentage : {percentage:.2f}% [{statistics}]")
-            __layout_overheads(target_faces, manhattan_distance, overheads)
+            # console.info(f"{target_side}> Zero-overhead percentage : {percentage:.2f}% [{statistics}]")
+            target_side_layouts[target_side] = __make_overheads_layout(target_faces, manhattan_distance, overheads)
+
+        for z in range(manhattan_distance, -manhattan_distance-1, -1):
+            rows = ""
+            for s in sides:
+                rows += f"{target_side_layouts[s][z]}"
+            console.info(rows)
+        console.info("")
