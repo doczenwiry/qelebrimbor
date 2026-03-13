b3c555d60072942c0b6a04413e83072d2dad9d26
Seweryn Dynerowicz <seweryn.dynerowicz@gmail.com>
2026-03-13 18:27:40 +0100

    Altered pathfinders into classes.

--- qelebrimbor/pathfinders/pathfinder_bfs.py
+++ qelebrimbor/pathfinders/pathfinder_bfs.py
@@ -1,57 +1,62 @@
 import logging as lgr
-
-from qelebrimbor.pathfinders.paths import Path
-
 console = lgr.getLogger(__name__)
 
-from collections import defaultdict, deque
+from collections import deque
 
+from qelebrimbor.pathfinders.paths import Path
 from qelebrimbor.common.components_bg import CubeKind
 from qelebrimbor.common.coordinates import Coordinates
 from qelebrimbor.helpers.blockgraph import BlockGraphHelper
 from qelebrimbor.helpers.spacetime import Spacetime
 
-def find_paths_bfs(
-    final: tuple[CubeKind, Coordinates], start: tuple[CubeKind, Coordinates] = (CubeKind.XZZ, Spacetime.ORIGIN),
-    extra_volume: int = 3
-) -> defaultdict[int, list[Path]]:
-    paths = defaultdict(list)
+class PathFinderBFS:
+    @staticmethod
+    def find_paths(
+        final: tuple[CubeKind, Coordinates], start: tuple[CubeKind, Coordinates] = (CubeKind.XZZ, Spacetime.ORIGIN),
+        extra_volume: int = 3
+    ) -> tuple[int, list[Path]]:
+        paths = []
 
-    start_kind, start_position = start
-    final_kind, final_position = final
+        start_kind, start_position = start
+        final_kind, final_position = final
 
-    console.info(f"Searching for paths from {start_kind}@{start_position} to {final_kind}@{final_position}.")
+        console.info(f"Searching for paths from {start_kind}@{start_position} to {final_kind}@{final_position}.")
 
-    maximal_volume = start_position.get_manhattan_distance(final_position) + extra_volume
+        minimal_volume = None
+        maximal_volume = start_position.get_manhattan_distance(final_position) + extra_volume
 
-    initial = Path( start, final )
-    queue: deque[Path] = deque([ initial ])
+        initial = Path( start, final )
+        queue: deque[Path] = deque([ initial ])
 
-    while queue:
-        path = queue.popleft()
-        kind, position = path.cubes[-1]
-        console.debug(f"Current : {kind}@{position}")
-        for next_kind, next_position in BlockGraphHelper.get_candidate_constellation(kind, position):
-            if path.contains(next_position):
-                continue
+        while queue:
+            path = queue.popleft()
+            kind, position = path.cubes[-1]
+            console.debug(f"Current : {kind}@{position}")
+            for next_kind, next_position in BlockGraphHelper.get_candidate_constellation(kind, position):
+                if path.contains(next_position):
+                    continue
 
-            if not Spacetime.in_octant(next_position):
-                continue
+                if not Spacetime.in_octant(next_position):
+                    continue
 
-            if next_kind in [ CubeKind.YYY , CubeKind.OOO ]:
-                continue
+                if next_kind in [ CubeKind.YYY , CubeKind.OOO ]:
+                    continue
 
-            extended: Path = path.copy()
-            extended.append(next_kind, next_position)
+                extended: Path = path.copy()
+                extended.append(next_kind, next_position)
 
-            if next_kind == final_kind and next_position == final_position:
-                console.debug(f"> Target reached : {next_kind}@{next_position}")
-                maximal_volume = extended.manhattan_length()
-                paths[ maximal_volume ].append( extended )
+                if next_kind == final_kind and next_position == final_position:
+                    console.debug(f"> Target reached : {next_kind}@{next_position}")
+                    if extended.manhattan_length() < maximal_volume:
+                        minimal_volume = extended.manhattan_length()
+                        maximal_volume = extended.manhattan_length()
+                        paths.clear()
+                    if extended.manhattan_length() == maximal_volume:
+                        paths.append( extended )
 
-            if extended.manhattan_length() <= maximal_volume:
-                console.debug(f"> {next_kind}@{next_position}")
+                if extended.manhattan_length() <= maximal_volume:
+                    console.debug(f"> {next_kind}@{next_position}")
 
-                queue.append( extended )
+                    queue.append( extended )
 
-    return paths
+        return minimal_volume, paths
--- qelebrimbor/pathfinders/pathfinder_dfs.py
+++ qelebrimbor/pathfinders/pathfinder_dfs.py
@@ -1,7 +1,6 @@
 import logging as lgr
 console = lgr.getLogger(__name__)
 
-from collections import defaultdict
 from queue import PriorityQueue
 
 from qelebrimbor.common.components_bg import CubeKind
@@ -11,50 +10,54 @@ from qelebrimbor.helpers.spacetime import Spacetime
 
 from qelebrimbor.pathfinders.paths import Path
 
-def find_paths_dfs(
-    final: tuple[CubeKind, Coordinates], start: tuple[CubeKind, Coordinates] = (CubeKind.XZZ, Spacetime.ORIGIN),
-    extra_volume: int = 3
-) -> defaultdict[int, list[Path]]:
-    paths = defaultdict(list)
+class PathFinderDFS:
+    @staticmethod
+    def find_paths(
+        final: tuple[CubeKind, Coordinates], start: tuple[CubeKind, Coordinates] = (CubeKind.XZZ, Spacetime.ORIGIN),
+        extra_volume: int = 3
+    ) -> tuple[int, list[Path]]:
+        paths = []
 
-    start_kind, start_position = start
-    final_kind, final_position = final
+        start_kind, start_position = start
+        final_kind, final_position = final
 
-    minimal_manhattan_distance = start_position.get_manhattan_distance(final_position) + extra_volume
+        minimal_volume = None
+        maximal_volume = start_position.get_manhattan_distance(final_position) + extra_volume
 
-    initial = Path( start , final )
-    queue: PriorityQueue = PriorityQueue()
-    queue.put( initial )
+        initial = Path( start , final )
+        queue: PriorityQueue = PriorityQueue()
+        queue.put( initial )
 
-    console.info(f"Searching for paths from {start_kind}@{start_position} to {final_kind}@{final_position}.")
+        console.info(f"Searching for paths from {start_kind}@{start_position} to {final_kind}@{final_position}.")
 
-    while not queue.empty():
-        path = queue.get()
-        kind, position = path.cubes[-1]
-        console.debug(f"Current : {kind}@{position}")
-        for next_kind, next_position in BlockGraphHelper.get_candidate_constellation(kind, position):
-            if path.contains(next_position):
-                continue
+        while not queue.empty():
+            path = queue.get()
+            kind, position = path.cubes[-1]
+            console.debug(f"Current : {kind}@{position}")
+            for next_kind, next_position in BlockGraphHelper.get_candidate_constellation(kind, position):
+                if path.contains(next_position):
+                    continue
 
-            if next_kind in [ CubeKind.YYY , CubeKind.OOO ]:
-                continue
+                if next_kind in [ CubeKind.YYY , CubeKind.OOO ]:
+                    continue
 
-            extended: Path = path.copy()
-            extended.append(next_kind, next_position)
+                extended: Path = path.copy()
+                extended.append(next_kind, next_position)
 
-            if extended.has_reached_target():
-                manhattan_distance = extended.manhattan_length()
-                if manhattan_distance < minimal_manhattan_distance:
-                    minimal_manhattan_distance = manhattan_distance
-                    paths.clear()
+                if extended.has_reached_target():
+                    manhattan_length = extended.manhattan_length()
+                    if manhattan_length < maximal_volume:
+                        maximal_volume = manhattan_length
+                        minimal_volume = manhattan_length
+                        paths.clear()
 
-                if manhattan_distance == minimal_manhattan_distance:
-                    console.info(f"> Target reached : {next_kind}@{next_position} [{extended.manhattan_length()}]")
-                    console.debug(f">> {extended}")
-                    paths[ manhattan_distance ].append( extended )
+                    if manhattan_length == maximal_volume:
+                        console.info(f"> Target reached : {next_kind}@{next_position} [{extended.manhattan_length()}]")
+                        console.debug(f">> {extended}")
+                        paths.append( extended )
 
-            if extended.manhattan_length() + extended.manhattan_distance_remaining() < minimal_manhattan_distance:
-                console.debug(f"> {next_kind}@{next_position}")
-                queue.put( extended )
+                if extended.manhattan_length() + extended.manhattan_distance_remaining() < maximal_volume:
+                    console.debug(f"> {next_kind}@{next_position}")
+                    queue.put( extended )
 
-    return paths
+        return minimal_volume, paths
