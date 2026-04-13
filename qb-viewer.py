3bf60f9a8fa75d5f5c9157fb522f4a4b48276923
Seweryn Dynerowicz <seweryn.dynerowicz@gmail.com>
2026-04-13 16:55:32 +0200

    Added computation of excess volume required by edges in the VZX.
    - Obtained by looking at how many extra cubes are needed to realise an edge.

--- qb-viewer.py
+++ qb-viewer.py
@@ -1,5 +1,6 @@
 import sys
 
+from qelebrimbor.common.components_bg import CubeKind
 from qelebrimbor.volumetric_zx_graph import VolumetricZxGraph
 from qelebrimbor.vedo.vzx_viewer import VolumetricZxGraphViewer
 
@@ -16,6 +17,17 @@ if __name__ == '__main__':
 
     vzx: VolumetricZxGraph = VolumetricZxGraph.from_file(filepath)
     vzx.log_summary()
+
+    excess_volume = 0
+    for edge in vzx.edges:
+        volume = len(vzx.get_edge_realisation(*edge)) - 1
+        if volume > 0:
+            console.info(f"Edge {edge} [+v={volume}] : {vzx.get_edge_realisation(*edge)}")
+        excess_volume += volume
+
+    boundaries = len(list(vzx.get_cubes(cube_kind = CubeKind.OOO)))
+    console.info(f"Total volume : {vzx.number_of_cubes() - boundaries}")
+    console.info(f"Excess volume: {excess_volume}")
 
     viewer = VolumetricZxGraphViewer(vzx, label = filepath)
     viewer.display()
