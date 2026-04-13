40dd69295021b83c83b6e4507a6b73668b5295de
Seweryn Dynerowicz <seweryn.dynerowicz@gmail.com>
2026-04-13 15:13:16 +0200

    Altered qb-viewer to work with filepath passed as argument.

--- qb-viewer.py
+++ qb-viewer.py
@@ -1,18 +1,21 @@
+import sys
+
 from qelebrimbor.volumetric_zx_graph import VolumetricZxGraph
 from qelebrimbor.vedo.vzx_viewer import VolumetricZxGraphViewer
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
-logging.getLogger('qelebrimbor.utils').setLevel(logging.INFO)
-logging.getLogger('qelebrimbor.helpers').setLevel(logging.CRITICAL)
-logging.getLogger("qelebrimbor.volumetric_zx_graph").setLevel(logging.DEBUG)
-logging.getLogger('qelebrimbor.vedo').setLevel(logging.INFO)
+logging.basicConfig(level=logging.INFO)
+console = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    circuit_name = "random_42_5_10"
-    vzx: VolumetricZxGraph = VolumetricZxGraph.from_file(f"assets/vzx/{circuit_name}.vzx")
+    if len(sys.argv) < 2:
+        raise Exception("Filepath to a *.vzx file required.")
 
+    filepath = sys.argv[1]
+    console.info(f"Visualisation of {filepath}.")
+
+    vzx: VolumetricZxGraph = VolumetricZxGraph.from_file(filepath)
     vzx.log_summary()
 
-    viewer = VolumetricZxGraphViewer(vzx, label = circuit_name)
+    viewer = VolumetricZxGraphViewer(vzx, label = filepath)
     viewer.display()
