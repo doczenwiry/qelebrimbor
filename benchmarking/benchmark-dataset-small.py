1716556283b5c1522f02a3fdac79139a9cf37c67
Seweryn Dynerowicz <seweryn.dynerowicz@gmail.com>
2026-05-02 09:15:54 +0200

    Corrected detection of dataset for benchmarking.

--- benchmarking/benchmark-dataset-small.py
+++ benchmarking/benchmark-dataset-small.py
@@ -31,26 +31,30 @@ DATASET = 'small'
 DATASET_DIRECTORY = f"../benchmarking/datasets/{DATASET}"
 
 random.seed(42)
-SEEDS = [ int(random.random() * 4242424242) for _ in range(10) ]
+SEED_COUNT = 10
+SEEDS = [ int(random.random() * 4242424242) for _ in range(SEED_COUNT) ]
 QUBITS = DATASET_PARAMETERS[DATASET]['QUBITS']
 DEPTHS = DATASET_PARAMETERS[DATASET]['DEPTHS']
 
+def get_dataset_filepaths() -> list[str]:
+    return sorted(list(map(
+        lambda parameters: f"random-cnots-q{parameters[1]}-d{parameters[2]}-s{parameters[0]}.json",
+        itertools.product(SEEDS, QUBITS, DEPTHS)
+    )))
+
 def dataset_detected():
-    return len(os.listdir(DATASET_DIRECTORY)) > 0
+    present_inputs = set(filter(lambda name: name.endswith(".json"), os.listdir(DATASET_DIRECTORY)))
+    dataset_inputs = set(get_dataset_filepaths())
+
+    return dataset_inputs.issubset(present_inputs)
 
 def generate_dataset():
-    if dataset_detected():
-        print(f"Existing dataset found in {DATASET_DIRECTORY}. Aborting generation.")
-        exit(0)
-
-    print(f"Seeds generated : {SEEDS}")
+    print(f"Generating dataset using seeds : {SEEDS}")
 
     for seed, qubits, depth in itertools.product(SEEDS, QUBITS, DEPTHS):
         random.seed(seed)
         circuit = f"random-cnots-q{qubits}-d{depth}-s{seed}"
         zx = pyzx.generate.cnots(qubits = qubits, depth = depth)
-
-        print(f"Generating circuit {circuit}")
 
         with open(f"{DATASET_DIRECTORY}/{circuit}.json", 'w') as file:
             file.write(zx.to_json())
@@ -60,6 +64,8 @@ if __name__ == "__main__":
 
     if not dataset_detected():
         generate_dataset()
+    else:
+        print(f"Existing dataset found in {DATASET_DIRECTORY}.")
 
-    for input_file in os.listdir(DATASET_DIRECTORY):
-        os.system(f"python ../qb.py -s {DATASET_DIRECTORY}/{input_file}")
+    for input_file in filter(lambda name: name.endswith(".json"), os.listdir(DATASET_DIRECTORY)):
+        os.system(f"python ../qb.py -s {DATASET_DIRECTORY}/{input_file} 2> /dev/null")
