fe03c4d4d8abf22cb98d1689d17e316ce152499f
Seweryn Dynerowicz <seweryn.dynerowicz@gmail.com>
2026-05-09 17:30:11 +0200

    Corrected painting of VdPipe of type Hadamard when the endpoints have the same color.

--- qelebrimbor/vedo/bg_painter/shaded.py
+++ qelebrimbor/vedo/bg_painter/shaded.py
@@ -36,13 +36,11 @@ class ShadedBlockGraphPainter(BlockGraphPainter):
         # cellcolors are for faces (+X, -X, +Y, -Y, +Z, -Z)
         colors = []
         distances = target.position - source.position
-        source_type = source.kind.get_type()
-        target_type = target.kind.get_type()
         for c in range(3):
             color = ZxPalette.LGRAY
             if distances[c] == 0:
-                if source_type == target_type:
-                    color = ZxPalette.get_minor(source_type)
+                if source.kind == target.kind:
+                    color = ZxPalette.get_minor(source.kind.get_type())
                 else:
                     color = ZxPalette.LGRAY
             colors.append(color)
