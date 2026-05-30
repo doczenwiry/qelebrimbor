a5e6b60d8b7843fab284fbdf0d1d3561ac180cec
Seweryn Dynerowicz <seweryn.dynerowicz@gmail.com>
2026-05-30 10:28:23 +0200

    Corrected unit tests for ColorShuffling, added commutativity test.

--- tests/unit/test_color_shufflings.py
+++ tests/unit/test_color_shufflings.py
@@ -23,7 +23,7 @@ class TestColorShuffling(TestCase):
     def test_identity(self):
         identity = ColorShuffling("xyz")
         for elt in ColorShuffling.generate():
-            self.assertEqual(elt, elt.append(identity))
+            self.assertEqual(elt, elt.extend(identity))
             self.assertEqual(elt, identity.extend(elt))
 
     def test_extend(self):
@@ -31,15 +31,26 @@ class TestColorShuffling(TestCase):
         testing = testing.extend(ColorShuffling.convert(SpacetimeHelper.YP))
         print(f"Testing : {testing}")
 
+    def test_commutativity(self):
+        elements = ColorShuffling.generate()
+        for elt1, elt2 in product(elements, repeat=2):
+            commutative_l = elt1.extend(elt2)
+            commutative_r = elt2.extend(elt1)
+            self.assertEqual(
+                commutative_l,
+                commutative_r,
+                msg=f"{elt1} * {elt2} != {elt2} * {elt1}",
+            )
+
     def test_associativity(self):
         elements = ColorShuffling.generate()
         for elt1, elt2, elt3 in product(elements, repeat=3):
-            associative_l = elt1.append(elt2).append(elt3)
-            associative_r = elt1.append(elt2.append(elt3))
+            associative_l = elt1.extend(elt2).extend(elt3)
+            associative_r = elt1.extend(elt2.extend(elt3))
             self.assertEqual(
                 associative_l,
                 associative_r,
-                msg=f"({elt1} * {elt2}) * {elt3} != {elt1} * ({elt2} * {elt3}) / {elt1.append(elt2)}",
+                msg=f"({elt1} * {elt2}) * {elt3} != {elt1} * ({elt2} * {elt3}) / {elt1.extend(elt2)}",
             )
 
     def test_other(self):
