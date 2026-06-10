6e8035b2a48d09790311e6df941b3bf66f66e13e
Seweryn Dynerowicz <seweryn.dynerowicz@gmail.com>
2026-06-10 08:10:25 +0200

    Replaced hardcoded parameters for benchmark-dataset-rings.py with parsed arguments.

--- benchmarking/benchmark.py
+++ benchmarking/benchmark.py
@@ -20,12 +20,6 @@ from enum import Enum
 
 import pyzx
 
-random.seed(42)
-SEED_COUNT = 10
-SEEDS = [int(random.random() * 4242424242) for _ in range(SEED_COUNT)]
-
-BENCHMARK_DIRECTORY = pathlib.Path(__file__).resolve().parent
-
 
 class Dataset(Enum):
     SMALL = 0
@@ -33,50 +27,59 @@ class Dataset(Enum):
     LARGE = 2
 
 
-DATASET_PARAMETERS: dict[str, dict[str, list[int]]] = {
-    Dataset.SMALL.name: {"QUBITS": [4], "DEPTHS": [4, 8, 16, 32, 64, 128, 256, 512, 1024]}
-}
+class Benchmark:
+    __QUICK_COUNT: int = 10
+    __ROBUST_COUNT: int = 100
+    __DATASET_PARAMETERS: dict[str, dict[str, list[int]]] = {
+        Dataset.SMALL.name: {"QUBITS": [4], "LAYERS": [4, 8, 16, 32, 64, 128, 256, 512, 1024]}
+    }
 
+    def __init__(self, dataset: Dataset, robust: bool = False, hadamards: bool = False):
+        random.seed(42)
+        self.seeds = [
+            int(random.random() * 4242424242)
+            for _ in range(Benchmark.__ROBUST_COUNT if robust else Benchmark.__QUICK_COUNT)
+        ]
+        self.hadamards = hadamards
+        self.qubits = Benchmark.__DATASET_PARAMETERS[dataset.name]["QUBITS"]
+        self.layers = Benchmark.__DATASET_PARAMETERS[dataset.name]["LAYERS"]
+        dataset_part = f"datasets/{dataset.name.lower()}"
+        hadamard_part = "hadamard" if self.hadamards else "identity"
+        self.__directory = str(pathlib.Path(__file__).resolve().parent) + "/" + dataset_part + "/" + hadamard_part
 
-def get_dataset_parameters(dataset: Dataset):
-    return itertools.product(
-        DATASET_PARAMETERS[dataset.name]["QUBITS"], DATASET_PARAMETERS[dataset.name]["DEPTHS"], SEEDS
-    )
+    @property
+    def directory(self) -> str:
+        return self.__directory
 
+    @property
+    def parameters(self):
+        return itertools.product(self.qubits, self.layers, self.seeds)
 
-def get_dataset_directory(dataset: Dataset, hadamards: bool = False) -> str:
-    return f"{BENCHMARK_DIRECTORY}/datasets/{dataset.name.lower()}/" + ("hadamard" if hadamards else "identity")
-
-
-def get_dataset_filenames(dataset: Dataset) -> list[str]:
-    return list(
-        map(
-            lambda parameters: (
-                f"random-cnots-q{parameters[0]}-d{parameters[1]}-s{parameters[2]}.pyzx.json"  # noqa: E501
-            ),
-            get_dataset_parameters(dataset),
+    @property
+    def filenames(self) -> list[str]:
+        return list(
+            map(
+                lambda parameters: f"random-cnots-q{parameters[0]}-d{parameters[1]}-s{parameters[2]}.pyzx.json",
+                self.parameters,
+            )
         )
-    )
 
+    def dataset_detected(self):
+        present_inputs = set(filter(lambda name: name.endswith(".pyzx.json"), os.listdir(self.directory)))
+        dataset_inputs = set(self.filenames)
 
-def dataset_detected(dataset: Dataset, hadamards: bool = False):
-    directory = get_dataset_directory(dataset, hadamards)
-    present_inputs = set(filter(lambda name: name.endswith(".pyzx.json"), os.listdir(directory)))
-    dataset_inputs = set(get_dataset_filenames(dataset))
+        return dataset_inputs.issubset(present_inputs)
 
-    return dataset_inputs.issubset(present_inputs)
+    def generate_dataset(self):
+        directory = self.directory
+        for qubits, depth, seed in self.parameters:
+            random.seed(seed)
+            filename = f"random-cnots-q{qubits}-d{depth}-s{seed}"
+            if self.hadamards:
+                circuit = pyzx.generate.CNOT_HAD_PHASE_circuit(qubits=qubits, depth=depth, p_had=0.2, p_t=0.0)
+                zx = circuit.to_graph()
+            else:
+                zx = pyzx.generate.cnots(qubits=qubits, depth=depth)
 
-
-def generate_dataset(dataset: Dataset, hadamards: bool = False):
-    directory = get_dataset_directory(dataset, hadamards)
-    for qubits, depth, seed in get_dataset_parameters(dataset):
-        random.seed(seed)
-        filename = f"random-cnots-q{qubits}-d{depth}-s{seed}"
-        if hadamards:
-            circuit = pyzx.generate.CNOT_HAD_PHASE_circuit(qubits=qubits, depth=depth, p_had=0.2, p_t=0.0, seed=seed)
-            zx = circuit.to_graph()
-        else:
-            zx = pyzx.generate.cnots(qubits=qubits, depth=depth)
-
-        with open(f"{directory}/{filename}.pyzx.json", "w") as file:
-            file.write(zx.to_json())
+            with open(f"{directory}/{filename}.pyzx.json", "w") as file:
+                file.write(zx.to_json())
